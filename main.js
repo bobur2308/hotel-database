fc74929d1f11db3680ae21287c696cdb0518f850
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2023-03-26 00:04:12 +0500

    changes

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express()
-const port = process.env.PORT || 5500
-const exphbs = require('express-handlebars')
-const path = require('path')
-const connectDB = require('./db/db')
-require('dotenv').config()
-connectDB()
+const app = express();
+const port = process.env.PORT || 5500;
+const exphbs = require('express-handlebars');
+const path = require('path');
+const connectDB = require('./db/db');
+require('dotenv').config();
+connectDB();
 const session = require('express-session');
 
 app.use(session({
@@ -14,13 +14,22 @@ app.use(session({
   saveUninitialized: false,
 }));
 
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
+// Create an instance of the exphbs engine
+const hbs = exphbs.create({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials')
+});
 
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.engine('.hbs', exphbs.engine({extname:'.hbs'}))
-app.set('view engine', '.hbs')
-app.use('/',require('./Routes/Routes'))
+// Register the exphbs engine with express
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
-app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
+app.use('/',require('./Routes/Routes'));
+
+app.listen(port, ()=> console.log('> Server is up and running on port : ' + port));
